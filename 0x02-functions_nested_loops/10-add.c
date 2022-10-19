c38cd5249f57e43479d482cd43bae2bc0ceaed4c
henryokaforng <henryokaforng@gmail.com>
2022-10-18 19:21:47 -0700

    tasks nested functions koading

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+Task 11
+
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
+
